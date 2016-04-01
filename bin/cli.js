8b149011218c98ee3d5948ee0523e43cd7dedcc2
Romain Beaumont <romain.rom1@gmail.com>
2016-04-01 16:47:44 +0200

    add verbose option

--- bin/cli.js
+++ bin/cli.js
@@ -3,14 +3,15 @@
 const fernflower=require("fernflower");
 const path = require('path');
 
-if(process.argv.length !=4) {
-  console.log("Usage : fernflower <jarFile> <outputDir>");
+if(process.argv.length <4 || process.argv.length >5) {
+  console.log("Usage : fernflower <jarFile> <outputDir> [<verbose>]");
   process.exit(1);
 }
 
 const inputJar=process.argv[2];
 const outputDir=process.argv[3];
+const verbose=process.argv[4];
 
-fernflower(inputJar,outputDir)
+fernflower(inputJar,outputDir,verbose)
   .then((decompiledDir) => console.log("Decompiled "+inputJar+" at "+decompiledDir))
   .catch(err => console.log(err.stack));
--- index.js
+++ index.js
@@ -6,14 +6,32 @@ const promisify = require("es6-promisify");
 const mkdirp=promisify(require("mkdirp"));
 const extract = promisify(require('extract-zip'));
 
-function runFernflower(inputDir,outputDir)
+function runFernflower(inputDir,outputDir,showProgress)
 {
   var fernflowerProcess=spawn('java', [
     '-jar',
     path.join(__dirname,"fernflower.jar"),
     inputDir,
-    outputDir
-  ]);
+    outputDir],
+    {
+      stdio: 'pipe'
+    });
+
+  fernflowerProcess.stdout.setEncoding('utf8');
+  fernflowerProcess.stderr.setEncoding('utf8');
+  var buffer = "";
+  fernflowerProcess.stdout.on('data', onData);
+  fernflowerProcess.stderr.on('data', onData);
+
+  function onData(data) {
+    buffer += data;
+    var lines = buffer.split("\n");
+    var len = lines.length - 1;
+    for(var i = 0; i < len; ++i) {
+      if(showProgress) console.log(lines[i]);
+    }
+    buffer = lines[lines.length - 1];
+  }
 
   return new Promise((resolve)=> {
     fernflowerProcess.on('exit', function() {
@@ -22,13 +40,13 @@ function runFernflower(inputDir,outputDir)
   });
 }
 
-module.exports=function(inputJar,outputDir)
+module.exports=function(inputJar,outputDir,showProgress)
 {
   var compiledDir=path.join(outputDir,"compiled");
   var decompiledDir=path.join(outputDir,"decompiled");
   return mkdirp(compiledDir)
     .then(() => mkdirp(decompiledDir))
     .then(() => extract(inputJar, {dir: compiledDir}))
-    .then(() => runFernflower(compiledDir, decompiledDir))
+    .then(() => runFernflower(compiledDir, decompiledDir,showProgress))
     .then(() => decompiledDir);
 };
